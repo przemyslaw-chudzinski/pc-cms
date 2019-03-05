9f06305ab3221f19b87562a186d6714f9558c3d4
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-03-05 16:43:46 +0100

    some changes

--- app/Core/MassActions.php
+++ app/Core/MassActions.php
@@ -8,9 +8,8 @@ class MassActions
     public function setMassActions($module_name = '', $action_route_name = null, array $args = [])
     {
         if ($module_name === '') throw new Exception('Module name is required');
-        if ($action_route_name === null) {
-            $action_route_name = 'mass_actions';
-        }
+        if ($action_route_name === null) $action_route_name = 'mass_actions';
+
         return view('admin.material_theme.components.massActions.massActions', [
             'args' => $args,
             'module_name' => $module_name,
